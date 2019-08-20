3ca1f2ba430238ca78f18ce0d3ec373da7791436
xck <Chaokai_xu@163.com>
2019-08-20 09:03:08 +0800

    数组的引用，一个小tip

---
+++ src/main/java/cn/nh/kevin/Reference.java
@@ -0,0 +1,21 @@
+package cn.nh.kevin;
+
+/**
+ * 标题:测试数组的引用对象
+ * 描述:
+ * 版权: 税友软件集团股份有限公司
+ * 作者: xck
+ * 时间: 2019-08-19 09:07
+ */
+public class Reference {
+	public static void main(String[] args) {
+		int []data = {1,2,3,4};
+		int []test = data;
+		for (int i = 0; i < test.length; i++) {
+			test[i] = 6;
+		}
+		for (int i = 0; i < data.length; i++) {
+			System.out.println(data[i]);
+		}
+	}
+}
