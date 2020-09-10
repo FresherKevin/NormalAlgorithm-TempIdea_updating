fff183c80be94c90566e4f79e70140664c29210c
kevin <chaokai_xu@163.com>
2020-09-10 13:38:41 +0800

    还是加上spring+mvc框架吧

---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: xck
+  Date: 2020/9/9
+  Time: 16:21
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Index</title>
+</head>
+<body>
+this is index!
+</body>
+</html>
--- src/test/java/cn/nh/kevin/TestAbs.java
+++ src/test/java/cn/nh/kevin/TestAbs.java
@@ -1,7 +1,15 @@
 package cn.nh.kevin;
 
+import cn.nh.kevin.DTO.BigDecimalDTO;
 import cn.nh.kevin.TestAbstractImpl.SonSample;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * <p> 名称：TestAbstractImpl </p>
@@ -16,11 +24,24 @@ public class TestAbs {
 	public static void main(String[] args) {
 		SonSample sonSample = new SonSample();
 		sonSample.saySomething();
-
 		try {
 			double num = 5/0;
 		} catch (Exception e) {
 			log.error("异常:{}",e);
 		}
+	}
+	@Test
+	public void myTest(){
+		List<BigDecimalDTO> arrayList = new ArrayList();
+		arrayList.add(new BigDecimalDTO("a", BigDecimal.valueOf(3L)));
+		arrayList.add(new BigDecimalDTO("b", BigDecimal.valueOf(2L)));
+		arrayList.add(new BigDecimalDTO("a", BigDecimal.valueOf(1L)));
+		arrayList.add(new BigDecimalDTO("d", BigDecimal.valueOf(6L)));
+		BigDecimal bqSrehj = arrayList.stream().map(BigDecimalDTO::getValue).reduce(BigDecimal.ZERO, BigDecimal::add);
+
+
+		Map<String, List<BigDecimalDTO>> sbmap = arrayList.stream()
+				.collect(Collectors.groupingBy(BigDecimalDTO::getName));
+		System.out.println();
 	}
 }
